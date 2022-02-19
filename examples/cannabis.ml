fd4b53a2b2eeaf7adf05864995fbf6eadf7856f2
Sébastien Patte <sebastien.patte@universite-paris-saclay.fr>
2022-02-19 14:47:13 +0100

    example "cannabis" : add CPS particle filter execution

--- examples/cannabis.ml
+++ examples/cannabis.ml
@@ -5,6 +5,17 @@ open Distribution
 let () =
   Arg.parse Basic.speclist (fun _ -> ()) Basic.usage_msg;
 
+  open Basic.Enum_sampling
+
+let hmm _prob () =
+  let m = 1.0 in
+  let log = Owl_stats.gaussian_pdf ~mu:m ~sigma:1.0 1.2 in
+  log
+  
+let _ = 
+  Format.printf "gaussian logpdf : %f\n" (hmm {old=[||]; cur=[||]; scores=[||];id_exec=0;id_sample=0} ());
+  
+
   
 open Basic.Enum_sampling
 
@@ -64,3 +75,18 @@ let _ =
     Format.printf "%d " v
   done;
   Format.printf "@."
+
+open Cps_operators
+open Infer.Importance_sampling
+
+let cannabis () = 
+  let* smoke = sample (bernoulli ~p:0.6) in
+  let* coin = sample (bernoulli ~p:0.5) in
+  let* () = assume (coin=1 || smoke=1) in
+  return smoke
+
+let _ =
+  Format.printf "@.-- Cannabis, CPS Importance Sampling --@.";
+  let dist = infer cannabis () in
+  let { values; probs; _ } = get_support ~shrink:true dist in
+  Array.iteri (fun i x -> Format.printf "%d %f@." x probs.(i)) values
